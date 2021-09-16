d2791db330fd49b632789c3b5ca657a3b103063b
chrislemus <icrislemus@gmail.com>
2021-09-16 18:24:34 -0400

    create separate module for route handling

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  if (url == '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="test" name="message"><button type="submit">Send</button></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    });
-    res.writeHead(302, { location: '/' });
-    // code below is same as above
-    // res.statusCode = 302
-    // res.setHeader('Location', '/')
-    return res.end();
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
